c85cf06001b99ccc178a1cf33d763f11b9bea64b
Zaali Mohamed <zaali.mohamed3002@gmail.com>
2025-05-07 16:54:01 +0100

    Updating gitignore and requirements.txt

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ load_dotenv()  # Load environment variables from .env file
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL'  )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
